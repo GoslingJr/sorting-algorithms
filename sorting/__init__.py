d1094058add6fd09c6e50929edcccb6c2ef7de0d
Запорожец глеб <148813894+GoslingJr@users.noreply.github.com>
2025-12-28 18:37:33 +0300

    Delete sorting directory

--- sorting/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bubble_sort import bubble_sort
-from .heap_sort import heap_sort
-from .merge_sort import merge_sort
-from .quick_sort import quick_sort
-
-__version__ = "1.0.0"
-__all__ = ["bubble_sort", "heap_sort", "merge_sort", "quick_sort"]

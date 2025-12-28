9a7a9731868c894ad5a5174747b6e0d636313550
Запорожец глеб <148813894+GoslingJr@users.noreply.github.com>
2025-12-28 18:40:43 +0300

    Create test_heap_sort.py

---
+++ tests/test_heap_sort.py
@@ -0,0 +1,25 @@
+import unittest
+from sorting.heap_sort import heap_sort
+
+class TestHeapSort(unittest.TestCase):
+    
+    def test_empty_list(self):
+        self.assertEqual(heap_sort([]), [])
+    
+    def test_single_element(self):
+        self.assertEqual(heap_sort([5]), [5])
+    
+    def test_sorted_list(self):
+        self.assertEqual(heap_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+    
+    def test_reverse_sorted(self):
+        self.assertEqual(heap_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+    
+    def test_duplicates(self):
+        self.assertEqual(heap_sort([3, 1, 2, 1, 3]), [1, 1, 2, 3, 3])
+    
+    def test_negative_numbers(self):
+        self.assertEqual(heap_sort([-5, -1, -3, 0, 2]), [-5, -3, -1, 0, 2])
+
+if __name__ == '__main__':
+    unittest.main()
